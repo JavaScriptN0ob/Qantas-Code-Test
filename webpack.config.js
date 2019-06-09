05cc1bd545845a01420400b330bb33fa4e0be5ce
Long Zhao <zlong@outlook.com>
2019-06-09 14:26:35 +1000

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var BUILD_DIR  = path.resolve(__dirname, 'build/');
-var SOURCE_DIR = path.resolve(__dirname, 'src/');
+const BUILD_DIR = path.resolve(__dirname, 'build/');
+const SOURCE_DIR = path.resolve(__dirname, 'src/');
 
 module.exports = {
-  entry: SOURCE_DIR + '/index.js',
+  entry: `${SOURCE_DIR}/index.jsx`,
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [{
@@ -21,18 +21,18 @@ module.exports = {
       test: /\.(png|jpg|gif)$/,
       use: [{
         loader: 'url-loader',
-        options:{
+        options: {
           limit: 8192,
-        }
+        },
       }],
     }, {
       test: /\.(js|jsx)$/,
       use: ['babel-loader'],
-    }]
+    }],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: SOURCE_DIR + '/index.html',
+      template: `${SOURCE_DIR}/index.html`,
     }),
-  ]
+  ],
 };
